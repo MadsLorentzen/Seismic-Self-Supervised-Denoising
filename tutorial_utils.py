9da24bfc4084bea00728a1f9d07fd4dbe314f231
Ruslan Miftakhov <38415261+RMiftakhov@users.noreply.github.com>
2022-07-25 07:52:34 +0300

    Initial commit for the video

--- tutorial_utils.py
+++ tutorial_utils.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 from torch.utils.data import TensorDataset, DataLoader 
+from tqdm import tqdm
+
 
 
 def regular_patching_2D(data, 
@@ -496,3 +498,214 @@ def plot_field_results(noisy,
 
     fig.tight_layout()
     return fig,axs
+
+def multi_active_pixels(patch, 
+                        num_activepixels, 
+                        neighbourhood_radius=5,
+                       ):
+    """ Function to identify multiple active pixels and replace with values from neighbouring pixels
+    
+    Parameters
+    ----------
+    patch : numpy 2D array
+        Noisy patch of data to be processed
+    num_activepixels : int
+        Number of active pixels to be selected within the patch
+    neighbourhood_radius : int
+        Radius over which to select neighbouring pixels for active pixel value replacement
+    Returns
+    -------
+        cp_ptch : numpy 2D array
+            Processed patch 
+        mask : numpy 2D array
+            Mask showing location of active pixels within the patch 
+    """
+
+    n_rad = neighbourhood_radius  # descriptive variable name was a little long
+
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
+    # STEP ONE: SELECT ACTIVE PIXEL LOCATIONS
+    idx_aps = np.random.randint(0, patch.shape[0], num_activepixels)
+    idy_aps = np.random.randint(0, patch.shape[1], num_activepixels)
+    id_aps = (idx_aps, idy_aps)
+    
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
+    # STEP TWO: SELECT NEIGHBOURING PIXEL LOCATIONS
+    
+    # PART 1: Compute Shift
+    # For each active pixel compute shift for finding neighbouring pixel and find pixel
+    x_neigh_shft = np.random.randint(-n_rad // 2 + n_rad % 2, n_rad // 2 + n_rad % 2, num_activepixels)
+    y_neigh_shft = np.random.randint(-n_rad // 2 + n_rad % 2, n_rad // 2 + n_rad % 2, num_activepixels)
+    
+    # OPTIONAL: don't allow replacement with itself
+    for i in range(len(x_neigh_shft)):
+        if x_neigh_shft[i] == 0 and y_neigh_shft[i] == 0:
+            # This means its replacing itself with itself...
+            shft_options = np.trim_zeros(np.arange(-n_rad // 2 + 1, n_rad // 2 + 1))
+            x_neigh_shft[i] = np.random.choice(shft_options[shft_options != 0], 1)
+
+    # PART 2: Find x and y locations of neighbours for the replacement
+    idx_neigh = idx_aps + x_neigh_shft
+    idy_neigh = idy_aps + y_neigh_shft    
+    # Ensure neighbouring pixels within patch window
+    idx_neigh = idx_neigh + (idx_neigh < 0) * patch.shape[0] - (idx_neigh >= patch.shape[0]) * patch.shape[0]
+    idy_neigh = idy_neigh + (idy_neigh < 0) * patch.shape[1] - (idy_neigh >= patch.shape[1]) * patch.shape[1]
+    # Get x,y of neighbouring pixels
+    id_neigh = (idx_neigh, idy_neigh)
+    
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
+    # STEP THREE: REPLACE ACTIVE PIXEL VALUES BY NEIGHBOURS
+    cp_ptch = patch.copy()
+    cp_ptch[id_aps] = patch[id_neigh]
+    
+    # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
+    # STEP FOUR: MAKE ACTIVE PIXEL MASK
+    # Make mask and corrupted patch
+    mask = np.ones_like(patch)
+    mask[id_aps] = 0.
+
+    return cp_ptch, mask
+
+def n2v_train(model, 
+              criterion, 
+              optimizer, 
+              data_loader, 
+              device):
+    """ Blind-spot network training function
+    
+    Parameters
+    ----------
+    model : torch model
+        Neural network
+    criterion : torch criterion
+        Loss function 
+    optimizer : torch optimizer
+        Network optimiser
+    data_loader : torch dataloader
+        Premade data loader with training data batches
+    device : torch device
+        Device where training will occur (e.g., CPU or GPU)
+    
+    Returns
+    -------
+        loss : float
+            Training loss across full dataset (i.e., all batches)
+        accuracy : float
+            Training RMSE accuracy across full dataset (i.e., all batches) 
+    """
+    
+    model.train()
+    accuracy = 0  # initialise accuracy at zero for start of epoch
+    loss = 0  # initialise loss at zero for start of epoch
+
+    for dl in tqdm(data_loader):
+        # Load batch of data from data loader 
+        X, y, mask = dl[0].to(device), dl[1].to(device), dl[2].to(device)
+        
+        optimizer.zero_grad()
+        
+        # Predict the denoised image based on current network weights
+        yprob = model(X)
+
+        #  Compute loss function only at masked locations and backpropogate it
+        ls = criterion(yprob * (1 - mask), y * (1 - mask))
+        ls.backward()        
+        
+        optimizer.step()
+        with torch.no_grad():
+            yprob = yprob
+            ypred = (yprob.detach().cpu().numpy()).astype(float)
+            
+        # Retain training metrics
+        loss += ls.item()  
+        accuracy += np.sqrt(np.mean((y.cpu().numpy().ravel( ) - ypred.ravel() )**2))  
+        
+    # Divide cumulative training metrics by number of batches for training
+    loss /= len(data_loader)  
+    accuracy /= len(data_loader)  
+
+    return loss, accuracy
+
+def n2v_evaluate(model,
+                 criterion, 
+                 optimizer, 
+                 data_loader, 
+                 device):
+    """ Blind-spot network evaluation function
+    
+    Parameters
+    ----------
+    model : torch model
+        Neural network
+    criterion : torch criterion
+        Loss function 
+    optimizer : torch optimizer
+        Network optimiser
+    data_loader : torch dataloader
+        Premade data loader with training data batches
+    device : torch device
+        Device where network computation will occur (e.g., CPU or GPU)
+    
+    Returns
+    -------
+        loss : float
+            Validation loss across full dataset (i.e., all batches)
+        accuracy : float
+            Validation RMSE accuracy across full dataset (i.e., all batches) 
+    """
+    
+    model.train()
+    accuracy = 0  # initialise accuracy at zero for start of epoch
+    loss = 0  # initialise loss at zero for start of epoch
+
+    for dl in tqdm(data_loader):
+        
+        # Load batch of data from data loader 
+        X, y, mask = dl[0].to(device), dl[1].to(device), dl[2].to(device)
+        optimizer.zero_grad()
+        
+        yprob = model(X)
+
+        with torch.no_grad():            
+            # Compute loss function only at masked locations 
+            ls = criterion(yprob * (1 - mask), y * (1 - mask))
+        
+            ypred = (yprob.detach().cpu().numpy()).astype(float)
+        
+        # Retain training metrics
+        loss += ls.item()  
+        accuracy += np.sqrt(np.mean((y.cpu().numpy().ravel( ) - ypred.ravel() )**2))  
+        
+    # Divide cumulative training metrics by number of batches for training
+    loss /= len(data_loader)  
+    accuracy /= len(data_loader)  
+
+    return loss, accuracy
+
+# Define a basic function to calculate frequency spectra
+# As we are using an fft we should really taper or window the data to prevent edge effects, 
+# but for this demonstration we are going to conveniently ignore this
+
+# An FFT returns a vector of complex values, in this case we are using the RFFT option to just return the real values 
+
+def fspectra(data, dt = 1):
+    """
+    Calculate the frequency spectra
+    """
+    # Amplitude values
+    
+    # Get the absolute value of the Fourier coefficients
+    fc = np.abs(np.fft.rfft(data, axis = -1))
+    print(fc.shape)
+    # Take the mean to get the amplitude values of the spectra
+    a = np.mean(fc, axis = 0)
+    print(a.shape)
+    # Get the frequency values corresponding to the coefficients
+    # We need the length of the window and the sample interval in seconds 
+                
+    dts = dt / 1000
+    length = data.shape[-1]
+                
+    f = np.fft.rfftfreq(length, d = dts)
+               
+    return f, a
